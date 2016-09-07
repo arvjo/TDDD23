a06477ac5747c4d475c6a44cbaaa70ccb36034eb
arvjo <riavd18@gmail.com>
2016-09-07 22:22:31 +0200

    funka

---
+++ Assets/Scripts/Direction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Direction : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rb;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+       
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float inputX = Input.GetAxis("Horizontal2");
+        //float inputY = Input.GetAxis("Vertical");
+        rb.MoveRotation(rb.rotation + speed * inputX);
+     
+	}
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed;// = new Vector2(10, 10);
+    public float torque;
+    private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();      
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+   
+        Vector3 movement = new Vector3(speed * inputX , speed * inputY, 0);
+       
+       /*
+        if (inputY > 0)
+        {
+            rb.AddForce(movement);
+        }*/
+
+        rb.AddForce(movement);
+        /*movement *= Time.deltaTime;
+        transform.Translate(movement);*/
+    }
+}
