46264bb2b80d0a17e7c55783eb028bab53605e12
arvjo <riavd18@gmail.com>
2016-09-12 15:40:10 +0200

    enemy and bullet
    enemy ai och döda fiender

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    public int damadge = 1;
+    // Use this for initialization
+    public bool isEnemyShoot = true;
+	void Start () {
+        Destroy(gameObject, 10);
+	}
+	
+}
---
+++ Assets/Scripts/EnemyAi.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAi : MonoBehaviour {
+    public GameObject player;
+    public float speed;
+    public float maxSpeed;
+    private Rigidbody2D playerRb;
+    private Rigidbody2D enemyRb;
+    // Use this for initialization
+    void Start () {
+        enemyRb = GetComponent<Rigidbody2D>();
+        playerRb = player.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        enemyRb.AddForce(playerRb.position - enemyRb.position);
+        if (enemyRb.velocity.magnitude > maxSpeed)
+        {
+            enemyRb.velocity = Vector3.ClampMagnitude(enemyRb.velocity, maxSpeed);
+        }
+    }
+}
