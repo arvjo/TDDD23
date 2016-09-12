262b3b6341b582acd2fc9c639f6152a40d6e46f9
arvjo <riavd18@gmail.com>
2016-09-12 11:46:07 +0200

    kör rakt fram och döda skott

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+    private int damadge = 1;
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 10);
+	}
+	
+}
