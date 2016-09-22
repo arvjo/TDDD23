de32c66fba51b415c3eff3adfcfe3d21e1f4798e
arvjo <riavd18@gmail.com>
2016-09-22 10:51:54 +0200

    nya nivåer och transitions

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,16 +8,21 @@ public class GameController : MonoBehaviour {
     public GameObject player;
     public Vector3 spawnValues;
     public int enemyCount;
+    private int enemyLives;
     public bool gameOver = false;
+    private int currentScene;
     // Use this for initialization
     void Start()
-    {      
-        spawnPlayer(); 
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        spawnPlayer();
+        enemyLives = enemyCount;
         StartCoroutine(spawnEnemies(enemyCount));
     }
 
     void spawnPlayer()
     {
+        
         Vector3 spawnPosition = new Vector3(1,1,1);
         Quaternion spawnRotation = Quaternion.identity;
         Instantiate(player, spawnPosition, spawnRotation);
@@ -42,17 +47,28 @@ public class GameController : MonoBehaviour {
         }
         
     }
- 
+    
+    public void setECount()
+    {
+        --enemyLives;
+    }
+
     public void GameOver()
     {
         gameOver = true;       
     }
+
 	
 	// Update is called once per frame
 	void Update () {
         if (gameOver == true)
         {
             SceneManager.LoadScene("GameOver");
+        }
+        if (enemyLives == 0)
+        {
+            ++currentScene;
+            SceneManager.LoadScene(currentScene);
         }
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,13 +6,14 @@ public class Health : MonoBehaviour {
     public int hp = 1;
     public bool isEnemy = true;
     private GameController gameController;
+    private int enemyCount;
 	// Use this for initialization
-   
+
 	void OnTriggerEnter2D(Collider2D collider)
     {
         Bullet projectile = collider.gameObject.GetComponent<Bullet>();
-
         gameController = GameObject.FindWithTag("GameController").GetComponent<GameController>();
+     
         if (projectile != null)
         {
        
@@ -23,10 +24,15 @@ public class Health : MonoBehaviour {
                 Destroy(projectile.gameObject);
                 if(hp == 0)
                 {
-                    Debug.Log("hej");
+                   
                     if(gameObject.tag == "Player")
                     {
-                        gameController.GetComponent<GameController>().GameOver();                    
+                        gameController.GameOver();                    
+                    }
+                    else
+                    {         
+                        gameController.setECount();
+                        
                     }
                     Destroy(gameObject);
                    
