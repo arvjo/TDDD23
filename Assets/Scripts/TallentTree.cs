f877d581440a8d59a4c6dd1632df1badcb9a5d97
arvjo <riavd18@gmail.com>
2016-09-22 17:12:45 +0200

    Påbörjade tallents

---
+++ Assets/Scripts/TallentTree.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TallentTree : MonoBehaviour {
+
+    private int bullets = 1;
+    private bool created;
+	// Use this for initialization
+    
+	void Awake () {
+        Debug.Log(created);
+        if (!created)
+        {
+            DontDestroyOnLoad(transform.gameObject);
+            created = true;
+        }else
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+	
+    public void setExtraBullets(string name)
+    {
+        ++bullets;
+        Debug.Log(bullets);
+    }
+
+    public void MenuButton(string menu)
+    {
+        SceneManager.LoadScene(menu);
+    }
+    public int getExtraBullets()
+    {
+        return bullets;
+    }
+	
+}
